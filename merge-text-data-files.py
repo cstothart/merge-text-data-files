4313e63540849d2ea3e96838a0ae1a2c0306aef9
cstothart <cary.stothart@gmail.com>
2020-02-06 10:08:24 -0600

    added progress so far, and refactored with class

--- merge-text-data-files.py
+++ merge-text-data-files.py
@@ -10,56 +10,74 @@ import argparse
 import glob
 import os
 
-def get_args():
-    desc = 'Vertically stack all text data files (either tab-delimited or CSV) \
-        in a given directory.'
-    parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('in_path', help='path to directory containing text \
-                        files')
-    parser.add_argument('out_path', help='output file pathname (for example, \
-                        merged.txt)')
-    parser.add_argument('--csv', help='make output CSV', action='store_true')
-    parser.add_argument('--file_col', help='add column with file names to \
-                        the output file', action='store_true')                    
-    args = parser.parse_args()
-    return(args)
+class NoDirectoryError(Exception):
+    pass
 
-def get_file_paths(dir_path):
-    print('looking for text files in ' +  dir_path)    
-    file_paths = (glob.glob(os.path.join(dir_path, '*.txt')) + 
-                  glob.glob(os.path.join(dir_path, '*.csv')))
-    print('found ' + str(len(file_paths)) + ' files to process')
-    return(file_paths)
+class FileMerger:
 
-def process_file(file_path, index, out_delim, file_col):
-    print('processing ' + file_path)
-    file = open(file_path, 'r')
-    first_row = True
-    for row in file:
-        if first_row:
-            first_row = False
-            if index > 0:
-                pass
-            else:
-                if file_col:
+    def __init__(self):
+        try:
+            self.args = self._get_args()
+        except NoDirectoryError as e:
+            print(e)
+        self.out_delim = ',' if(self.args.csv) else '\t'
+        self.file_col = True if(self.args.file_col) else False
+        self.file_paths = self._get_file_paths(self.args.in_path)
+        self.out_file = open(self.args.out_path, 'w')
+
+    def process_files(self):
+        for file_path in self.file_paths:
+            self._process_file(file_path, self.file_paths.index(file_path))
+        self.out_file.close()        
+
+    def _get_args(self):
+        desc = 'Vertically stack all text data files (either tab-delimited \
+                or CSV) in a given directory.'
+        parser = argparse.ArgumentParser(description=desc)
+        parser.add_argument('in_path', help='path to directory containing \
+                            text files')
+        parser.add_argument('out_path', help='output file pathname (for \
+                            example, merged.txt)')
+        parser.add_argument('--csv', 
+                            help='make output CSV', 
+                            action='store_true')
+        parser.add_argument('--file_col', help='add column with file names to \
+                            the output file', action='store_true')                    
+        args = parser.parse_args()
+        if(not os.path.isdir(args.in_path)):
+            raise NoDirectoryError('unable to find directory: ' + 
+                                   str(args.in_path))
         else:
-            print(row)
-    file.close()
+            return(args)
+
+    def _get_file_paths(self, dir_path):
+        print('looking for text files in ' +  dir_path)    
+        file_paths = (glob.glob(os.path.join(dir_path, '*.txt')) + 
+                      glob.glob(os.path.join(dir_path, '*.csv')))
+        print('found ' + str(len(file_paths)) + ' files to process')
+        return(file_paths)        
+
+    def _process_file(self, file_path, index):
+        print('processing ' + file_path)
+        file = open(file_path, 'r')
+        first_row = True
+        for in_row in file:
+            if first_row:
+                first_row = False
+                if index > 0:
+                    pass
+                else:
+                    out_row = self._process_row(in_row)
+            else:
+                print(in_row)
+        file.close()        
+
+    def _process_row(self, in_row):
+        pass
 
 def main():
-    args = get_args()
-    if(not os.path.isdir(args.in_path)):
-        print('unable to find directory: ' + str(args.in_path))
-        exit()
-    out_delim = ',' if(args.csv) else '\t'
-    file_paths = get_file_paths(args.in_path)
-    out_file = open(args.out_path, 'w')
-    for file_path in file_paths:
-        process_file(file_path, 
-                     file_paths.index(file_path), 
-                     out_delim, 
-                     args.file_col)
-    out_file.close()
+    fm = FileMerger()
+    fm.process_files()
 
 if __name__ == '__main__':
     main()
